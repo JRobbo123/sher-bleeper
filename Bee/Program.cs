b4132aae6dfc46c2c9a644c48687ab5c0f2462e7
velli <velli@AAAAAAAAAAAAAAA>
2023-12-08 23:41:47 +0000

    Add project files.

---
+++ Bee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 240240024; i++)
+            {
+                Console.WriteLine("robbo423 will never unblock sher bleeper");
+                Console.WriteLine("do you agree?");
+                if (Console.ReadLine() != "yes")
+                {
+                    Console.WriteLine("kys");
+                    Console.WriteLine();
+                    Console.WriteLine( );
+                }
+                else
+                {
+                    Console.WriteLine("good");
+                    break;
+                }
+            }
+        }
+    }
+}
